2deb54912c219deb57ddfd3c7e12bec39e25cced
Mihail <mihail@gmail.com>
2020-01-05 19:23:14 +0200

    Cange #12 in jenkins_file

--- jenkins_file/VM2_start.sh
+++ jenkins_file/VM2_start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 a=$(sudo docker ps | grep petclinic | cut -d' ' -f 1)
-echo "a="$a
-
+echo "ID container="$a
 if [ -n "${a}" ]; then
   echo "Conteiner runing"
-  sudo docker stop $a
-  else
-   echo "Conteiner do'nt run"
+  docker-compose -f docker-compose_petclinic.yml stop
+  docker-compose -f docker-compose_petclinic.yml rm -f
+ else
+  echo "Conteiner do'nt run"
 fi
-sudo docker run -p 8080:8080 -d 192.168.2.112:5000/petclinic:latest
-
-#sudo docker run -p 8080:8080 -d 10.23.27.6:5000/petclinic:latest
+docker rmi 192.168.2.112:5000/petclinic -f
+#docker rmi 10.23.27.6:5000/petclinic -f
+docker pull 192.168.2.112:5000/petclinic:latest
+#docker pull 10.23.27.6:5000/petclinic:latest
+docker-compose -f docker-compose_petclinic.yml up -d
