d363330bba152a01b73d8dff4979df807c6acc8e
Mihail <mihail@gmail.com>
2020-01-12 23:09:40 +0200

    Cange #6 d-b.jenkins

--- jenkins_file/VM2_start.sh
+++ jenkins_file/VM2_start.sh
@@ -9,8 +9,8 @@ if [ -n "${a}" ]; then
  else
   echo "Conteiner do'nt run"
 fi
-docker rmi 192.168.2.112:5000/petclinic -f
+#docker rmi 192.168.2.112:5000/petclinic -f
 #docker rmi 10.23.27.6:5000/petclinic -f
-docker pull 192.168.2.112:5000/petclinic:latest
-#docker pull 10.23.27.6:5000/petclinic:latest
+#docker pull 192.168.2.112:5000/petclinic:stable
+docker pull 10.23.27.6:443/petclinic:stable
 docker-compose -f docker-compose_petclinic.yml up -d
