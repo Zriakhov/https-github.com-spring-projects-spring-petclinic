d15a981d005faed35a77e283ffc36a0b1a0fd723
Mykhailo_Zriakhov@epam.com <Zriakhov_Mykhailo@epam.com>
2020-01-14 16:23:52 +0200

    Fix VM2_start

--- jenkins_file/VM2_start.sh
+++ jenkins_file/VM2_start.sh
@@ -9,8 +9,6 @@ if [ -n "${a}" ]; then
  else
   echo "Conteiner don't run"
 fi
-#docker rmi 192.168.2.112:5000/petclinic -f
-#docker rmi 10.23.27.6:5000/petclinic -f
-#docker pull 192.168.2.112:5000/petclinic:stable
+docker rmi 10.23.27.6:443/petclinic -f
 docker pull 10.23.27.6:443/petclinic:stable
 docker-compose -f docker-compose_petclinic.yml up -d
